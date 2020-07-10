7e8a7a5d0b601c3c9d3120651e5b25e8aa0971f1
Hello Lala <laura.abowd@gmail.com>
2020-07-10 15:39:45 -0500

    added porduction environtment for mail_form

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -297,4 +297,8 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 config.omniauth :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], scope: 'userinfo.email,userinfo.profile', skip_jwt: true
+
+
+# add code below for redirect_uri in Rails??
+# OmniAuth.config.full_host = Rails.env.production? ? 'http://teacherintech.io' : 'http://localhost:3000'
 end
